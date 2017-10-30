f5d70a880f34a3fa9a201e69c2e43783ee83fd9a
Rakuray <carloseduarcma@gmail.com>
2017-10-29 22:00:21 -0500

    add more xml

---
+++ WeBike/app/src/main/java/webike/webike/adaptadores/adaptador_home_notificacion.java
@@ -0,0 +1,49 @@
+package webike.webike.adaptadores;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import webike.webike.R;
+import webike.webike.logic.Publicacion;
+
+/**
+ * Created by Carlos on 29/10/2017.
+ */
+
+public class adaptador_home_notificacion extends ArrayAdapter<Publicacion>{
+
+    public adaptador_home_notificacion(Context context , ArrayList<Publicacion> resource){
+        super(context , 0 , resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v= convertView;
+        if(v == null) {
+            LayoutInflater vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.adapterhomeNotificacion, null);
+        }
+
+        Publicacion r = getItem(position);
+        if (r!= null){
+            TextView txDestino = (TextView) v.findViewById(R.id.destino);
+            TextView txorigen = (TextView) v.findViewById(R.id.origen);
+            TextView txhora = (TextView) v.findViewById(R.id.hora);
+
+            txDestino.setText(r.getDestino());
+            txorigen.setText(r.getOrigen());
+            txhora.setText(r.getHora());
+        }
+        return v;
+
+    }
+}
--- WeBike/app/src/main/java/webike/webike/logic/Publicacion.java
+++ WeBike/app/src/main/java/webike/webike/logic/Publicacion.java
@@ -5,4 +5,50 @@ package webike.webike.logic;
  */
 
 public class Publicacion {
+    private String origen;
+    private String destino;
+    private String hora;
+    private String descripcion;
+
+    public Publicacion() {
+    }
+
+    public Publicacion(String origen, String destino, String hora, String descripcion) {
+        this.origen = origen;
+        this.destino = destino;
+        this.hora = hora;
+        this.descripcion = descripcion;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
